391f36e4bed59e9d34e182a3a849e9fc20e3852f
Akhilesh-yadav-ji <ay3238271@gmail.com>
2024-09-23 00:08:43 +0530

    to commit Akhilesh user

---
+++ workspace-spring-tool-suite-4-4.24.0.RELEASE/AKhileshyadav/src/jsdfhue/Hello.java
@@ -0,0 +1,12 @@
+package jsdfhue;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Akhilesh kaise ho bhai");
+		
+
+	}
+
+}
